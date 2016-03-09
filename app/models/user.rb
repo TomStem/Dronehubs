bea5b0f74b82422cb3e6ddd70a36f329a3064091
Iris Kramer <ick.kramer@gmail.com>
2016-03-09 11:30:24 +0100

    Change comment of requester to inquirer for updated terminology

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :bids, dependent: :destroy, through: :drones # as owner
   has_many :drones, dependent: :destroy # as owner
-  has_many :jobs, dependent: :destroy # as requester
+  has_many :jobs, dependent: :destroy # as inquirer
 end
   
