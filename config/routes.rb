c3d5bcf07a10170eb52e5615ef4f0a821b81e6e4
Tom Stemerding <tom@stemerdingmail.nl>
2016-03-09 10:49:34 +0100

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
 
-  namespace :profile do
-    get '/jobs', to: 'jobs#index'
-    get '/jobs/new', to: 'jobs#new'
-    post '/jobs/create', to: 'jobs#create'
-    post '/jobs/:id/edit', to: 'jobs#edit'
-    patch '/bids/:id/accept', to: 'bids#accept'
+  devise_for :users
+
+  resources :jobs, only: [:index, :show] do
+    resources :bids, only: [:new, :create]
+  end
+
+  resources :owners, only: [:index, :show]
+
+  namespace :inquirer do
+    resources :jobs do
+      resources :bids, only: [] do
+        member do
+          patch :accept
+        end
+      end
+    end
   end
 
   namespace :owner do
-    get '/', to: 'users#owners'
-    get '/:id', to: 'users#show_owners'
-    get '/:id/bids', to: 'bids#show'
-    get '/drones/new', to: 'drones#new'
-    post '/drones/create', to: 'drones#create'
-    get '/drones/:id', to: 'drones#show'
+    resource :profile, only: [:show, :edit, :update]
+
+    resources :drones
   end
 end
