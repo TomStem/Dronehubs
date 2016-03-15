530fea58603dc3d471f9287485cf6675b219063b
Roy van Arkel <royvanarkel@msn.com>
2016-03-15 15:08:40 +0100

    Add basic dashboard structure

--- app/controllers/owner/profiles_controller.rb
+++ app/controllers/owner/profiles_controller.rb
@@ -1,6 +1,23 @@
 class Owner::ProfilesController < ApplicationController
   def show
     @owner = current_user
+    @bids_amount = 0
+    @jobs_amount = 0
+    @jobs = []
+
+    @owner.drones.each do |drone|
+      @bids_amount += drone.bids.size
+    end
+
+    @owner.drones.each do |drone|
+      drone.bids.each do |bid|
+        if bid.status == "accepted"
+          @jobs << bid.job
+          @jobs_amount += 1
+        end
+      end
+    end
+
   end
 
   def edit
