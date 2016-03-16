59a2b6bb244c6f9f55df1cb2ce60753d9dee4466
Tom Stemerding <tom@stemerdingmail.nl>
2016-03-16 17:13:56 +0100

    add terms page, route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+skip_before_action :authenticate_user!, only: [:home, :terms]
 
   def home
     @latest_jobs = Job.latest
     @latest_owners = User.latest
+  end
+
+  def terms
   end
 end
