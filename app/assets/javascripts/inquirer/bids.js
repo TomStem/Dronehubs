58e31f70150f929c42a31e87b5b19195b7f86a09
Iris Kramer <ick.kramer@gmail.com>
2016-03-18 15:40:58 +0100

    Add modal for placebid form

--- app/assets/javascripts/inquirer/bids.js
+++ app/assets/javascripts/inquirer/bids.js
@@ -1,10 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-  $(".js-jobs-new-bid").click(function(){
+  $(".new-bid").click(function(){
     var job_id = $(this).data("job-id");
-    var form = $('.js-jobs-new-bid-form[data-job-id="' + job_id + '"]');
-    $(form).show();
-    return false;
+    console.log($('#modal-bid form').attr('action', '/jobs/' + job_id + '/bids'))
   });
 });
