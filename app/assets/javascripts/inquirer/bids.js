bc68adcc7d668b6a4fbe146652ed76cada06195e
Iris Kramer <ick.kramer@gmail.com>
2016-03-18 13:49:14 +0100

    Add placebid route to job listing page

--- app/assets/javascripts/inquirer/bids.js
+++ app/assets/javascripts/inquirer/bids.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $(".js-jobs-new-bid").click(function(){
+    var job_id = $(this).data("job-id");
+    var form = $('.js-jobs-new-bid-form[data-job-id="' + job_id + '"]');
+    $(form).show();
+    return false;
+  });
+});
