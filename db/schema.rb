ee0166054a6f2a7bab80c9c7314cd49c8fc01490
Iris Kramer <ick.kramer@gmail.com>
2016-03-21 12:18:39 +0100

    DB migrate the categories and small changes

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20160321105852) do
   create_table "bids", force: :cascade do |t|
     t.integer  "amount"
     t.text     "description"
-    t.string   "status",      default: "open"
+    t.string   "status"
     t.integer  "job_id"
     t.integer  "drone_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "bids", ["drone_id"], name: "index_bids_on_drone_id", using: :btree
@@ -61,10 +61,10 @@ ActiveRecord::Schema.define(version: 20160321105852) do
     t.text     "description"
     t.string   "location"
     t.date     "date"
-    t.string   "status",      default: "open"
+    t.string   "status"
     t.integer  "user_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "title"
   end
 
@@ -81,19 +81,19 @@ ActiveRecord::Schema.define(version: 20160321105852) do
   add_index "user_categories", ["user_id"], name: "index_user_categories_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "owner",                  default: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "owner"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "profile_pic"
