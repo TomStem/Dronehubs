0c6bbf5938f65d0309db0b8864c518500fd04462
Tom Stemerding <tom@stemerdingmail.nl>
2016-03-23 11:42:00 +0100

    fix cta bug

--- app/assets/javascripts/cta.js
+++ app/assets/javascripts/cta.js
@@ -1,21 +1,21 @@
-function TermsAndConditions(){
+function CallToAction(){
    days=30;
    myDate = new Date();
    myDate.setTime(myDate.getTime()+(days*24*60*60*1000));
-   document.cookie = 'TermsAndConditions=Accepted; expires=' + myDate.toGMTString();
+   document.cookie = 'CallToAction=Accepted; expires=' + myDate.toGMTString();
 }
 
 var cookie = document.cookie.split(';')
     .map(function(x){ return x.trim().split('='); })
-    .filter(function(x){ return x[0]==='TermsAndConditions'; })
+    .filter(function(x){ return x[0]==='CallToAction'; })
     .pop();
 
 if(cookie && cookie[1]==='Accepted') {
-    $("#terms-and-conditions").hide();
+  $("#call-to-action").hide();
 }
 
 $('.accept-button a').on('click', function(){
-    TermsAndConditions();
-     $("#terms-and-conditions").hide();
+    CallToAction();
+     $("#call-to-action").hide();
     return false;
 }); 
