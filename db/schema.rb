749b12c81f99f94bd716772514539e3e08d98802
Tom Stemerding <tom@stemerdingmail.nl>
2016-03-24 09:33:55 +0100

    new version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321105852) do
+ActiveRecord::Schema.define(version: 20160323142410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20160321105852) do
   create_table "bids", force: :cascade do |t|
     t.integer  "amount"
     t.text     "description"
-    t.string   "status"
+    t.string   "status",      default: "open"
     t.integer  "job_id"
     t.integer  "drone_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   add_index "bids", ["drone_id"], name: "index_bids_on_drone_id", using: :btree
@@ -61,14 +61,24 @@ ActiveRecord::Schema.define(version: 20160321105852) do
     t.text     "description"
     t.string   "location"
     t.date     "date"
-    t.string   "status"
+    t.string   "status",      default: "open"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "title"
   end
 
   add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "user_categories", force: :cascade do |t|
     t.integer  "category_id"
@@ -81,19 +91,19 @@ ActiveRecord::Schema.define(version: 20160321105852) do
   add_index "user_categories", ["user_id"], name: "index_user_categories_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "owner"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "owner",                  default: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "profile_pic"
